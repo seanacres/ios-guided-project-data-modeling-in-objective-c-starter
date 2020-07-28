23db28a5f158ba8ae6da31051bfd6cdb20214d26
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-27 21:38:07 -0400

    Added firstresponder class

---
+++ Quakes-Objc/FirstResponder.h
@@ -0,0 +1,17 @@
+//
+//  FirstResponder.h
+//  Quakes-Objc
+//
+//  Created by Sean Acres on 7/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstResponder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/FirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  FirstResponder.m
+//  Quakes-Objc
+//
+//  Created by Sean Acres on 7/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "FirstResponder.h"
+
+@implementation FirstResponder
+
+@end
